6b141f60270eacd6221086ec85493acb58653e58
David DeCorso <davidadecorso@gmail.com>
2020-07-15 17:18:24 -0400

    Updated game UI, color scheme, added structures and structure content

---
+++ scripts/structureContent.js
@@ -0,0 +1,55 @@
+/**
+ * Total number of structures
+ */
+const numStructures = 2;
+
+/**
+ * Types of structures
+ */
+const structureTypeEnum = Object.freeze({
+  ECONOMY: 0,
+  MILITARY: 1,
+  MOVEMENT: 2,
+  PRODUCTION: 3,
+});
+
+/**
+ *
+ */
+
+const structureModifierEnum = Object.freeze({
+  scaleMovementCost: 0,
+  setMovementCost: 1,
+  militaryProdModifier: 2,
+});
+
+/**
+ * Array of structure objects
+ */
+
+Structures = [
+  {
+    name: "Barracks",
+    effect: "units produced 2 turns quicker.",
+    type: structureTypeEnum.PRODUCTION,
+    time: 4,
+    modifierType: structureModifierEnum.militaryProdModifier,
+    modifier: 2,
+  },
+  {
+    name: "Road",
+    effect: "1/2 movement cost.",
+    type: structureTypeEnum.MOVEMENT,
+    time: 2,
+    modifierType: structureModifierEnum.scaleMovementCost,
+    modifier: 0.5,
+  },
+  {
+    name: "Bridge",
+    effect: "movement cost = 1.",
+    type: structureTypeEnum.MOVEMENT,
+    time: 3,
+    modifierType: structureModifierEnum.setMovementCost,
+    modifier: 1,
+  },
+];
